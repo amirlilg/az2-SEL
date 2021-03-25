0c83faf313bac39ffc03c76c5575c5b5f472f494
emad_salehi <emad.s1178@yahoo.com>
2021-03-25 23:22:19 +0430

    Test added for getter and setters

--- src/test/RectangleTest.java
+++ src/test/RectangleTest.java
@@ -10,4 +10,18 @@ public class RectangleTest {
         double area = rectangle.calculateArea();
         Assert.assertEquals(200.0, area, 0.0);
     }
+
+    @Test
+    public void widthChangeTest() {
+        Rectangle rectangle = new Rectangle(10, 20);
+        rectangle.setWidth(5.0);
+        Assert.assertEquals(5.0, rectangle.getWidth(), 0.0);
+    }
+
+    @Test
+    public void heightChangeTest() {
+        Rectangle rectangle = new Rectangle(10, 20);
+        rectangle.setHeight(25.0);
+        Assert.assertEquals(25.0, rectangle.getHeight(), 0.0);
+    }
 }
