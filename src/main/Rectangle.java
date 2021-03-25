4ce85afdb42209502865d51f2b80e4427738c33c
emad_salehi <emad.s1178@yahoo.com>
2021-03-25 23:44:12 +0430

    Square class and polygon interface added

--- src/main/Rectangle.java
+++ src/main/Rectangle.java
@@ -1,32 +1,33 @@
 package main;
 
-public class Rectangle{
-	private double width;
-	private double height;
-	
-	public Rectangle(double width, double height) {
-		this.width = width;
-		this.height = height;
-	}
-	
-	public double calculateArea() {
-		return width * height;
-	}
-	
-	public double getWidth() {
-		return this.width;
-	}
-	
-	public void setWidth(double width) {
-		this.width = width;
-	}
-	
-	public double getHeight() {
-		return this.height;
-	}
-	
-	public void setHeight(double height) {
-		this.height = height;
-	}
-	
+public class Rectangle implements Polygon {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+
+    public double getWidth() {
+        return this.width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
 }
---
+++ src/main/Square.java
@@ -0,0 +1,22 @@
+package main;
+
+public class Square implements Polygon{
+    private double side;
+
+    public Square(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public double calculateArea() {
+        return side * side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+}
