fde917aead37f482451234d701f10540ecc355f7
amirlilg <amiraligatmiri@gmail.com>
2021-03-25 23:31:04 +0430

    added set/get funcs for rectangle

--- src/main/Rectangle.java
+++ src/main/Rectangle.java
@@ -12,4 +12,21 @@ public class Rectangle{
 	public double calculateArea() {
 		return width * height;
 	}
+	
+	public double getWidth() {
+		return this.width;
+	}
+	
+	public void setWidth(double width) {
+		this.width = width;
+	}
+	
+	public double getHeight() {
+		return this.height;
+	}
+	
+	public void setHeight(double height) {
+		this.height = height;
+	}
+	
 }
